fcdfc45cda5fcd9cc599ab89b8f1e09fc5814cb7
Deathfab <chiarandini.christopher@gmail.com>
2023-04-18 16:21:41 +0200

    Amélioration PAge Chapitre

--- src/routes/Chapitres/components/TabChap.tsx
+++ src/routes/Chapitres/components/TabChap.tsx
@@ -1,5 +1,6 @@
-import PropTypes from 'prop-types'
+import { useState } from 'react'
 import '../../../styles/TabChap.css'
+
 
 
 interface Props  {
@@ -11,24 +12,32 @@ interface Props  {
 
 
 export default function TabChap({ chapNbr, chapTtl, chapDscr }: Props) {
+
+  const [forOpen, setForOpen] = useState(false)
+  
   return (
+   
     <div>
       <ul>
         <li>
-          <h2>Chapitre {chapNbr}</h2>
+          <h2>Chapitre {chapNbr}</h2> 
+          {forOpen ?
+          <button onClick={() => setForOpen(false)}>Fermer</button> : null}
         </li>
-
         <li>
-          <div className="tabChap">
-            <div>
-              <h3>{chapTtl}</h3>
-            </div>
-            <div>{chapDscr}</div>
+          {forOpen ?
+          <div className="tabChap" >
+          <div>
+            <h3>{chapTtl}</h3>
           </div>
+          <div>{chapDscr}</div>
+        </div>: null}
+          
         </li>
       </ul>
+      <button onClick={() => setForOpen(true)}>Ouvrir</button>
     </div>
-  )
+  ) 
 }
 
 
